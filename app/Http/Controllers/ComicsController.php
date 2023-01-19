7cdc37bc0d35da156998799dfa8b77bb018160c3
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-19 18:50:54 +0100

    dump(Comics)

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicsController extends Controller
@@ -13,7 +14,16 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        return view('pages.comics.index');
+        $comics = Comic::All();
+
+        $data =
+        [
+            'comics' => $comics
+        ];
+
+        dd($data);
+
+        return view('pages.comics.index', $data);
     }
 
     /**

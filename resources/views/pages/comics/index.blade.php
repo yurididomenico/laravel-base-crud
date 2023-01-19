e7ae54bbe982908b11c63cfd6dc7e80aa3c51147
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-19 17:31:33 +0100

    Creazione Modello

---
+++ resources/views/pages/comics/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title-page', 'test')
+
+@section('main-content')
+    <h1>Pagina Comics</h1>
+    {{-- Tutti i record della tabella comics --}}
+
+
+
+@endsection

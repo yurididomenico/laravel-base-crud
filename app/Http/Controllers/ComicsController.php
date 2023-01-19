67cea590d2d057e6fc20ed2c191c1b06695f532e
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-19 17:14:56 +0100

    layout app - header - route / -> named "home"

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -13,7 +13,7 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.comics.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@index');
+Route::get('/', 'PagesController@index')->name('home');
+
+Route::resource('/comics', ComicsController::class);
